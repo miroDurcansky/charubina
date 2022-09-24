fa08b077f77ce08a42c3717feccaf13c84c7f111
miroslav <durcansky.miroslav@gmail.com>
2022-09-24 12:05:59 +0200

    backend init

---
+++ charubinaBE/src/test/java/sk/charubina/springbootmongodbdocker/SpringbootMongodbDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.charubina.springbootmongodbdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
